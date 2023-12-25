7314985e4f2db093a9064f789554d6525f43c701
nadhem naili <nadhemnaili@gmail.com>
2023-12-25 11:20:10 +0100

    L'ORM Eloquent

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,18 +20,65 @@ Route::get('/', function () {
 });
 Route::prefix("/blog")->name("blog.")->group(function(){
     Route::get('/', function () {
-        return [
-            "link"=>\route('blog.show',["slug"=>"nadhem-27","id"=>13])
-        ];
+        // ****** add un post  *******
+        $posts = new Post();
+        $posts->title = "Mon second article";
+        $posts->slug = "mon-second-article-2";
+        $posts->content = "Mon contenu";
+        $posts->save();
+
+        return $posts;
+        
+
+
+
+        /** return all post */
+
+            // return Post::all();
+        
+        /** return des donnee spécifique  */
+       // $posts = Post::all(["id","title"]);
+
+        //return first element
+        //return $posts->first() ;
+
+       // return Post::all(["id","title"]);
+       //$posts = Post::find(1);
+
+       //**** utlisie pagination */
+            // $posts = Post::paginate(2);
+            //$posts = Post::paginate(2,["id","title"]);
+
+          /*  $posts = Post::find(1);
+            $posts->title = 'Nouveau titre';
+            //$posts->save();
+            $posts->delete();*/
+
+
+            // create post with fn create
+            /*$posts = Post::create([
+                "title"=>"Mon nouveau titre",
+                "slug"=>"mon-nouveau-test",
+                "content"=>"Mon nouveau contenu"
+            ]);*/
+
+            // update poste
+            /*$posts = Post::where("id",">","1")->update([
+                "title"=>"Mon nouveau titre *****",
+                "slug"=>"mon-nouveau-test",
+                "content"=>"Mon nouveau contenu"
+            ]);*/
+            //delete
+            //$posts = Post::where("id",">","1")->delete();
+            dump($posts);
+            die();
+       return $posts;
+
     })->name('blog.index');
     
     Route::get("/{slug}-{id}",function(string $slug,string $id,Request $request){
-        return [
-            "slug" => $slug,
-            "id" => $id,
-            "name"=> $request->input("name","John Doe"),
-    
-        ];
+       return Post::findOrFail($id);
+       
     })->where([
         'slug' => "[a-z0-9/-]+",
         'id'=>"[0-9]+"
