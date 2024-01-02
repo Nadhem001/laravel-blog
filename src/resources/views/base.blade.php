baa55e4aa6a5ddafa3fb1f0841d0fd401b9e163d
nadhem naili <nadhemnaili@gmail.com>
2024-01-02 11:00:26 +0100

    crud bien

--- src/resources/views/base.blade.php
+++ src/resources/views/base.blade.php
@@ -16,17 +16,14 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/">Blog</a>
+          <a class="navbar-brand" href="/">Agence</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a @class(['nav-link', 'active' => str_starts_with($routeName,'blog.index')]) aria-current="page" href="{{ route('blog.index') }}">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a @class(['nav-link', 'active' => str_starts_with($routeName,'blog.index')]) aria-current="page" href="{{ route('blog.index') }}">Nos biens</a>
               </li>
             </ul>
             <div class="navbar-nav ms-auto mb-2 mb-lg-0">
